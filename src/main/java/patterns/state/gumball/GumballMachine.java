f0fb4bd2ca39722362b541a88deb5bb05aa9c676
Alexey Elnikov <frey87@yandex.ru>
2017-08-30 20:12:11 +0300

    add state pattern

---
+++ src/main/java/patterns/state/gumball/GumballMachine.java
@@ -0,0 +1,82 @@
+package patterns.state.gumball;
+
+
+public class GumballMachine {
+    // 4 состояния
+    final static int SOLD_OUT = 0;
+    final static int NO_QUARTER = 0;
+    final static int HAS_QUARTER = 0;
+    final static int SOLD = 3;
+    // текущее состоние, по умолчанию нет шариков
+    int state = SOLD_OUT;
+    // кол-во шариков в автомате
+    int count = 0;
+
+    public GumballMachine(int count) {
+        this.count = count;
+        if (count > 0) state = NO_QUARTER;
+    }
+
+    // бросаем монетку в автомат
+    public void insertQuarter() {
+        if (state == HAS_QUARTER) {  // если в автомате уже есть монетка
+            System.out.println("Вы не можете вставить еще одну монетку");
+        } else if (state == NO_QUARTER) {
+            state = HAS_QUARTER;
+            System.out.println("Вы засунули монетку");
+        } else if (state == SOLD_OUT){
+            System.out.println("Вы не можете вставить монетку, товара нет в наличии");
+        } else if (state == SOLD) {
+            System.out.println("Пожалуйста подождите, сейчас вы получите шарик с жвачкой");
+        }
+    }
+
+    // если покупатель пытается вернуть монетку
+    public void ejectQuarter() {
+        if (state == HAS_QUARTER) {
+            System.out.println("Деньги возвращены покупателю");
+            state = NO_QUARTER;
+        } else if (state == NO_QUARTER) {
+            System.out.println("Сначала вы должны засунуть монетку");
+        } else if (state == SOLD) {
+            System.out.println("Извините, но покупка уже была совершена");
+        } else if (state == SOLD_OUT) {
+            System.out.println("Вы не можете вернуть деньги, т.к. автомат не принимает деньги");
+        }
+    }
+
+    // покупатель дергает за рычаг
+    public void turnCrank() {
+        if (state == SOLD) {
+            System.out.println("Вы уже получили товар");
+        } else if (state == NO_QUARTER) {
+            System.out.println("Необходимо сначала положить монетку в автомат");
+        } else if (state == SOLD_OUT) {
+            System.out.println("Выдача невозможна, в автомате нет шариков");
+        } else if (state == HAS_QUARTER) {
+            System.out.println("Держите ваш шарик жвачки");
+            state = SOLD;
+            dispense();
+        }
+    }
+
+    // вызывается для выдачи шарика
+    public void dispense() {
+        if (state == SOLD) {
+            System.out.println("Выдача покупки");
+            count = count - 1;
+            if (count == 0) {
+                System.out.println("Шарики кончились");
+                state = SOLD_OUT;
+            } else {
+                state = NO_QUARTER;
+            }
+        } else if (state == NO_QUARTER) {
+            System.out.println("Сначала вы должны заплатить");
+        } else if (state == SOLD_OUT) {
+            System.out.println("Нет шариков для выдачи");
+        } else if (state == HAS_QUARTER) {
+            System.out.println("Нет шариков для выдачи");
+        }
+    }
+}
